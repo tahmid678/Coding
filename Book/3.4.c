977d9e6949b68da739e067ac090b2f6d3b66737d
Tahmid <tjawad98@gmail.com>
2021-03-18 20:32:31 +0600

    file changes

---
+++ Book/3.4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    FILE *fp;
+    char filename[]="my_file.txt";
+    fp=fopen(filename,"w");
+
+    fprintf(fp,"This is a file created by my program! ");
+    fprintf(fp,"I am so happy\n");
+    fclose(fp);
+
+    fp=fopen(filename,"w");
+    fprintf(fp,"Second line.\n");
+    fclose(fp);
+
+    return 0;
+}
