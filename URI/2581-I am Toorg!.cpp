c0decce43bfbf277809726cf84646e161f90edb4
Tahmid <tjawad98@gmail.com>
2021-04-21 15:59:46 +0600

    test

---
+++ URI/2581-I am Toorg!.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+int main(){
+    int tc;
+    char s[100];
+    cin>>tc;
+    getchar();
+
+    while(tc--){
+        gets(s);
+        cout<<"I am Toorg!"<<endl;
+    }
+
+    return 0;
+}
